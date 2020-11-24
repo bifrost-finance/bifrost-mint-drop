4afb02720a4b11a2332b386cbc104e864dde3658
Edwin Wang <lark930@gmail.com>
2020-11-24 14:56:20 +0800

    vETH support pausable

--- contracts/interfaces/IVETH.sol
+++ contracts/interfaces/IVETH.sol
@@ -5,4 +5,5 @@ pragma solidity ^0.6.0;
 interface IVETH {
     function mint(address account, uint amount) external;
     function burn(address account, uint amount) external;
+    function unpause() external;
 }
--- contracts/vETH.sol
+++ contracts/vETH.sol
@@ -4,10 +4,13 @@ pragma solidity 0.6.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract vETH is ERC20, Ownable {
+contract vETH is ERC20, Ownable, Pausable {
 
-    constructor() public ERC20("Voucher Ethereum", "vETH") Ownable() {}
+    constructor() public ERC20("Voucher Ethereum", "vETH") Ownable() Pausable() {
+        super._pause();
+    }
 
     function mint(address account, uint amount) external onlyOwner {
         super._mint(account, amount);
@@ -15,5 +18,19 @@ contract vETH is ERC20, Ownable {
 
     function burn(address account, uint amount) external onlyOwner {
         super._burn(account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(!super.paused(), "vETH: transfer while paused");
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(!super.paused(), "vETH: transfer while paused");
+        return super.transferFrom(sender, recipient, amount);
+    }
+
+    function unpause() external onlyOwner {
+        super._unpause();
     }
 }
