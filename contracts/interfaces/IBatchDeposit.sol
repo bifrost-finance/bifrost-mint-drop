66933947494e871bc9f43bda6a2461b4a6bda963
Edwin Wang <lark930@gmail.com>
2020-12-25 17:36:45 +0800

    add IBatchDeposit.sol

---
+++ contracts/interfaces/IBatchDeposit.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
+
+interface IBatchDeposit {
+
+    struct DepositArgs {
+        bytes pubkey; //48 bytes
+        bytes withdrawal_credentials; //32 bytes
+        bytes signature; //96 bytes
+        bytes32 deposit_data_root;
+    }
+
+    function hasDataFlags()  external view returns(bool[100] memory);
+
+    function changeWorker(address newWorker) external;
+
+    function changeMintDropOwner(address newOwner) external;
+
+    function lockMintDropWithdraw() external;
+
+    function unlockMintDropWithdraw() external;
+
+    function fillTheTable(DepositArgs[] memory args, uint256 start) external;
+
+    function doBatchDeposit(uint256 start, uint256 end) external;
+}
