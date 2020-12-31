73e8c7790ceb78cc8c76a7c5ef49b0ac721037ae
Edwin Wang <lark930@gmail.com>
2020-12-31 11:38:25 +0800

    update

--- contracts/interfaces/IBatchDeposit.sol
+++ contracts/interfaces/IBatchDeposit.sol
@@ -3,32 +3,6 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
-/*
-interface IBatchDeposit {
-
-    struct DepositArgs {
-        bytes pubkey; //48 bytes
-        bytes withdrawal_credentials; //32 bytes
-        bytes signature; //96 bytes
-        bytes32 deposit_data_root;
-    }
-
-    function hasDataFlags()  external view returns(bool[100] memory);
-
-    function changeWorker(address newWorker) external;
-
-    function changeMintDropOwner(address newOwner) external;
-
-    function lockMintDropWithdraw() external;
-
-    function unlockMintDropWithdraw() external;
-
-    function fillTheTable(DepositArgs[] memory args, uint256 start) external;
-
-    function doBatchDeposit(uint256 start, uint256 end) external;
-}
-*/
-
 interface IBatchDeposit {
 
     struct DepositArgs {
@@ -61,7 +35,3 @@ interface IBatchDeposit {
     function doBatchDeposit(uint256 start, uint256 end) external;
     
 }
-
-
-
-
